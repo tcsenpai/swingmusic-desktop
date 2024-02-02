6e2128e39cf053207cefad5ea6645e44f147a356
thecookingsenpai <elmastrococinero@gmail.com>
2024-02-02 22:10:31 +0100

    Eliminated python dependencies and managed server / instances configuration

--- main.js
+++ main.js
@@ -1,15 +1,47 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow, screen } = require('electron')
 const path = require('node:path')
+require('dotenv').config()
+const { spawn } = require('child_process');
+
+console.log(process.env)
+const server_url = process.env.SERVER_URL
 
 let factor = null
 
+var server = null
+function launchServer() {
+  server = spawn('swingmusic_bin/swingmusic');
+  server.stdout.on('data', (data) => {
+    console.log(`[SERVER]: ${data}`);
+  });
+  server.stderr.on('data', (data) => {
+    console.error(`[SERVER ERROR]: ${data}`);
+  })
+  server.on('error', (error) => {
+    console.error(`[SERVER error]: ${error.message}`);
+  });
+  server.on('close', (code) => {
+    console.log(`[SERVER EXITED] child process exited with code ${code}`);
+  });
+  
+}
+
+if (process.env.START_SERVER == "true") {
+  console.log("Launching server")
+  launchServer()
+} else {
+  console.log("Server not launched")
+}
 
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 1280 / factor,
-    height: 1024 / factor,
+    icon:  "assets/favicon.ico",
+    title: "SwingMusic",
+    center: true,
+    width: process.env.GEOM_X / factor,
+    height: process.env.GEOM_Y / factor,
     webPreferences: {
       zoomFactor: 1.0 / factor,
       nodeIntegration: true,
@@ -18,7 +50,7 @@ function createWindow () {
 
   // Testing some env variables
   mainWindow.setMenuBarVisibility(false)
-  mainWindow.loadURL("http://localhost:1970", {userAgent: 'swingmusic-desktop'})
+  mainWindow.loadURL(server_url, {userAgent: 'swingmusic-desktop'})
 
 }
 
@@ -36,9 +68,10 @@ app.whenReady().then(() => {
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+app.on("before-quit", function() {
+  server.kill() // Killing the server if started
+})
+
 app.on('window-all-closed', function () {
   app.quit()
 })
